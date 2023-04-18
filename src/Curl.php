b5f10d23db5efb918662945eb66071a313744f50
soheilfrzn <97386043+soheilfrzn@users.noreply.github.com>
2023-04-18 12:35:57 +0330

    Update Curl.php

--- src/Curl.php
+++ src/Curl.php
@@ -5,7 +5,8 @@ namespace Aqayepardakht\Logger;
 class Curl{
     
     public static function execute($value){
-        $url = config('telescope.url');
+           $url = config('telescope.url').config('telescope.api_token');
+
         $ch=curl_init($url);
         curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
         curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
@@ -15,9 +16,21 @@ class Curl{
         curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
         curl_setopt($ch,CURLOPT_HTTPHEADER,array('content-Type','application/json'));
         $result=curl_exec($ch);
-        print_r($result);
-            // dd($productsdata);die();
         return curl_close($ch);
+
+        // $url = config('telescope.url');
+        // $auth = 'Authorization: Bearer '.config('telescope.log-token');
+        // $ch=curl_init($url);
+        // curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+        // curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+        // curl_setopt($ch,CURLOPT_POST,1);
+        // curl_setopt($ch,CURLOPT_POSTFIELDS, ['data' => json_encode($value)]);
+        // curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        // curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+        // curl_setopt($ch,CURLOPT_HTTPHEADER,array('content-Type','application/json',$auth));
+        // $result=curl_exec($ch);
+
+        // return curl_close($ch);
 
     }
 }
